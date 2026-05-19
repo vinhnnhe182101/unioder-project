5188dc4ebf63bc093ea9888542c09e91df9eecc5
yingmod <vinhos51104@gmail.com>
2026-05-19 18:57:04 +0700

    remove fe

--- frontend/admin-portal/src/utils/image.js
+++
@@ -1,9 +0,0 @@
-export const resolveImageUrl = (url) => {
-    if (!url) return null;
-
-    const BASE_IMG_URL = import.meta.env.VITE_API_URL || 'http://localhost:80/api';
-
-    return url.startsWith('http')
-        ? url
-        : `${BASE_IMG_URL}/catalog${url}`;
-};
