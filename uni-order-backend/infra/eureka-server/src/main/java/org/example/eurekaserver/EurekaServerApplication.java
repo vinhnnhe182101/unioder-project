38ce1a96ec2b8dcb75c67d278f3d6179be8077f3
Mr.NoBody <thieuefvanwhieues@gmail.com>
2026-06-08 10:43:55 +0700

    refactor: tách api-gateway, service-registry, common-dto - chuyển api-gateway và service-registry sang thư mục infra/ và chuyển cấu hình sang file .env và config-repo - chuyển common-dto sang thư mục shared/

--- uni-order-backend/infra/eureka-server/src/main/java/org/example/eurekaserver/EurekaServerApplication.java
+++ uni-order-backend/infra/eureka-server/src/main/java/org/example/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package org.example.registry;
+package org.example.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ServiceRegistryApplication {
+public class EurekaServerApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceRegistryApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
