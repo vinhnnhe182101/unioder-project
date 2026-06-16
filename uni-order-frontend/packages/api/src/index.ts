80067d47737c299aaf83ef1a833acf1d847d6f23
Mr.NoBody <thieuefvanwhieues@gmail.com>
2026-06-16 20:41:48 +0700

    fix: đổi tên cho phù hợp với tên service userServiceApi

--- uni-order-frontend/packages/api/src/index.ts
+++ uni-order-frontend/packages/api/src/index.ts
@@ -1,14 +1,14 @@
 // 🌟 1. Khởi tạo cấu hình an toàn ban đầu cho SDK
 import {_registerUrlChangeListener, apiClient} from "./config/apiClient";
-import {identityServiceApi} from "./generated";
+import {userServiceApi} from "./generated";
 
 // Dùng let thay vì const để có thể thay đổi/cập nhật cấu hình động
-let apiConfig = new identityServiceApi.Configuration({
+let apiConfig = new userServiceApi.Configuration({
 	basePath: apiClient.defaults.baseURL,
 });
 
 // Khởi tạo instance controller ban đầu
-export let authApiClient = new identityServiceApi.IdentityControllerApi(
+export let authApiClient = new userServiceApi.UserControllerApi(
 	apiConfig,
 	apiClient.defaults.baseURL,
 	apiClient
@@ -17,12 +17,12 @@ export let authApiClient = new identityServiceApi.IdentityControllerApi(
 // 🌟 2. ĐĂNG KÝ LẮNG NGHE ĐỂ CẬP NHẬT ĐỘNG KHI CÓ CONFIG MỚI
 _registerUrlChangeListener((newUrl) => {
 	// Cập nhật lại Configuration Object
-	apiConfig = new identityServiceApi.Configuration({
+	apiConfig = new userServiceApi.Configuration({
 		basePath: newUrl,
 	});
 
 	// Ép instance của SDK sử dụng cấu hình và basePath mới ngay lập tức
-	authApiClient = new identityServiceApi.IdentityControllerApi(
+	authApiClient = new userServiceApi.UserControllerApi(
 		apiConfig,
 		newUrl,
 		apiClient
